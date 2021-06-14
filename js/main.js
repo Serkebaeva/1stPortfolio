f0fa7912c02c7c2998b026ecebc94109a536a4cc
AizadaSerkebaeva <aizada.serkebaeva.pls@gmail.com>
2021-06-14 09:21:24 +0600

    section Contacts

--- js/main.js
+++ js/main.js
@@ -1,6 +1,10 @@
 const menuToggler = document.querySelector(".menu-toggler");
 const menu = document.querySelector(".menu");
+const links = document.querySelectorAll(".menu__list");
 
-menuToggler.addEventListener("click", function () {
+menuToggler.addEventListener("click", () => {
   menu.classList.toggle("js-menu-show");
+  links.forEach((link) => {
+    link.classList.toggle("fade");
+  });
 });
